ed2673fb26e9609a10011532b907f5e55b18f3e1
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2023-12-08 09:32:37 +0100

    feat(string-utils): add capitalize function

---
+++ packages/utilities/src/lib/string/capitalize.function.spec.ts
@@ -0,0 +1,14 @@
+import { describe, test, expect } from '@jest/globals'
+import { capitalize } from './capitalize.function.js'
+
+describe('capitalize', () => {
+  test('when string length > 1', () => {
+    expect(capitalize('shiftcode')).toEqual('Shiftcode')
+  })
+  test('when string length = 1', () => {
+    expect(capitalize('s')).toEqual('S')
+  })
+  test('when string length < 1', () => {
+    expect(capitalize('')).toEqual('')
+  })
+})
---
+++ packages/utilities/src/lib/string/capitalize.function.ts
@@ -0,0 +1,3 @@
+export function capitalize(val: string): string {
+  return val.charAt(0).toUpperCase() + val.slice(1)
+}
