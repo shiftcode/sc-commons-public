a91f11aa6ebc5ea9ff58dd1e98203e39f9127180
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2023-12-08 09:51:58 +0100

    feat(promise-utils): add makeDeferred function

---
+++ packages/utilities/src/lib/promise/make-deferred.function.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from '@jest/globals'
+import { makeDeferred } from './make-deferred.function.js'
+
+describe('makeDeferred', () => {
+
+  test('returns an object containing a promise', () => {
+    const { promise } = makeDeferred()
+    expect(promise).toBeInstanceOf(Promise)
+  })
+
+  test('resolves the returned promise with the returned resolveFn', async () => {
+    const { promise, resolve } = makeDeferred()
+    resolve('foo')
+    await expect(promise).resolves.toBe('foo')
+  })
+
+  test('rejects the returned promise with the returned rejectFn', async () => {
+    const { promise, reject } = makeDeferred()
+    reject(new Error('Foo Bar'))
+    await expect(promise).rejects.toThrow('Foo Bar')
+  })
+
+})
