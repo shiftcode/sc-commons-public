a27601504cda2221dd0cdeda24ab24a029d4f8d0
Patrick Suter <patrick.suter@shiftcode.ch>
2024-07-12 11:38:25 +0200

    feat(branch-utilities): make eslint happy

--- packages/branch-utilities/src/lib/types/sc-override-env-var.type.ts
+++ packages/branch-utilities/src/lib/types/sc-override-env-var.type.ts
@@ -3,7 +3,9 @@ export interface CustomScOverrideEnv {
   // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents,@typescript-eslint/naming-convention
   SC_OVERRIDE: 'true' | any
   /** flag to override branchName for getBranchInfo(), needs to be used in conjunction with SC_OVERRIDE_IS_PR */
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents,@typescript-eslint/naming-convention
   SC_OVERRIDE_BRANCH_NAME: string | any
   /** flag to override isPr getBranchInfo(), needs to be used in conjunction with SC_OVERRIDE_IS_PR */
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents,@typescript-eslint/naming-convention
   SC_OVERRIDE_IS_PR: string | any
 }
