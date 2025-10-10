c99d8f907da6169d1daf77eb2bbd55570471a2da
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2025-10-10 11:58:56 +0200

    chore(eslint): use new flat eslint config

--- packages/eslint-plugin-rules/eslint.config.mjs
+++ packages/eslint-plugin-rules/eslint.config.mjs
@@ -1,41 +1,5 @@
-import baseConfig from '../../eslint.config.mjs';
+import { defineScTsConfig } from '@shiftcode/eslint-config-recommended'
 
-export default [
-  ...baseConfig,
-  {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: "module",
-      parserOptions: {
-        project: ["./tsconfig.json", "./tsconfig.jest.json"],
-      },
-    },
-    rules: {
-        "import/no-extraneous-dependencies": ["error", {
-            devDependencies: true,
-        }],
-    },
-  }
-];
-
-// export default [{
-//     languageOptions: {
-//         ecmaVersion: 5,
-//         sourceType: "script",
-
-//         parserOptions: {
-//             project: ["./tsconfig.json", "./tsconfig.jest.json"],
-//         },
-//     },
-// }, {
-//     files: ["**/*.ts"],
-//     plugins: {
-//         import: importPlugin,
-//     },
-//         rules: {
-//         "import/no-extraneous-dependencies": ["error", {
-//             devDependencies: true,
-//         }],
-//     },
-// }];
+export default defineScTsConfig({
+  languageOptions: { parserOptions: { project: ['./tsconfig.json', './tsconfig.jest.json'] } },
+})
