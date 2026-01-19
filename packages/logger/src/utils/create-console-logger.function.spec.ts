f1a97c61deb9bc64c5578e001d5e47a1a0d55bd3
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2026-01-19 17:02:40 +0100

    test(create-console-logger): test it

---
+++ packages/logger/src/utils/create-console-logger.function.spec.ts
@@ -0,0 +1,25 @@
+import { LogLevel } from '../model/log-level.enum.js'
+import { Logger } from '../model/logger.js'
+import { ConsoleJsonLogTransport } from '../transports/console-json-log-transport/console-json-log.transport.js'
+import { NodeConsoleLogTransport } from '../transports/node-console-log-transport/node-console-log.transport.js'
+import { createConsoleLogger } from './create-console-logger.function.js'
+
+describe('createConsoleLogger', () => {
+  it('should create logger with NodeConsoleLogTransport when type is "node"', () => {
+    const logger: Logger = createConsoleLogger('test-logger', LogLevel.INFO, 'node')
+
+    expect(logger).toBeDefined()
+    expect(logger['loggerTransports']).toHaveLength(1)
+    expect(logger['loggerTransports'][0]).toBeInstanceOf(NodeConsoleLogTransport)
+    expect(logger['loggerTransports'][0]['logLevel']).toBe(LogLevel.INFO)
+  })
+
+  it('should create logger with ConsoleJsonLogTransport when type is "json"', () => {
+    const logger: Logger = createConsoleLogger('test-logger', LogLevel.DEBUG, 'json')
+
+    expect(logger).toBeDefined()
+    expect(logger['loggerTransports']).toHaveLength(1)
+    expect(logger['loggerTransports'][0]).toBeInstanceOf(ConsoleJsonLogTransport)
+    expect(logger['loggerTransports'][0]['logLevel']).toBe(LogLevel.DEBUG)
+  })
+})
