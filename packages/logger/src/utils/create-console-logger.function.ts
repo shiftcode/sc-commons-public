a29a5c49cb6ab806b46005617f13948764f8c495
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2026-01-20 11:54:49 +0100

    Revert "feat(create-console-logger): default to ConsoleJsonLogTransport when in awsLambda"
    This reverts commit 9a2d0250ef066828fd9814006e341eafd9ef2673.

--- packages/logger/src/utils/create-console-logger.function.ts
+++ packages/logger/src/utils/create-console-logger.function.ts
@@ -4,18 +4,15 @@ import { Logger } from '../model/logger.js'
 import { BaseLoggerService } from '../services/base-logger.service.js'
 import { ConsoleJsonLogTransport } from '../transports/console-json-log-transport/console-json-log.transport.js'
 import { NodeConsoleLogTransport } from '../transports/node-console-log-transport/node-console-log.transport.js'
-import { isAwsLambdaEnv } from './is-aws-lambda-env.function.js'
 
 /**
  * Creates a simple {@link Logger} instance that logs to the console with the specified name and log level
 
  * @param name The name of the logger
  * @param logLevel decides the minimum log level to be logged
- * @param type decides whether to use {@link NodeConsoleLogTransport} or {@link ConsoleJsonLogTransport}.
- *   if not set, it will default to `json` when running in AWS Lambda environment, otherwise to `node`
+ * @param type decides whether to use {@link NodeConsoleLogTransport} or {@link ConsoleJsonLogTransport}
  */
-export function createConsoleLogger(name: string, logLevel: LogLevel = LogLevel.DEBUG, type?: 'node' | 'json'): Logger {
-  type ||= isAwsLambdaEnv() ? 'json' : 'node'
+export function createConsoleLogger(name: string, logLevel: LogLevel, type: 'node' | 'json'): Logger {
   let transport: LogTransport
   switch (type) {
     case 'node':
--- packages/logger/src/utils/is-aws-lambda-env.function.ts
+++
@@ -1,5 +0,0 @@
-
-export function isAwsLambdaEnv(): boolean {
-  // aws sets the AWS_EXECUTION_ENV
-  return typeof globalThis.process !== 'undefined' && !!globalThis.process.env?.['AWS_EXECUTION_ENV']
-}
