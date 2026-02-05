fc3efc70e8cb2eb06da563af719d77edd3e01cdf
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2026-02-05 14:05:09 +0100

    test: let tests run

--- packages/cdk-utilities/src/lib/utils/create-basic-auth-cf-fn.function.spec.ts
+++ packages/cdk-utilities/src/lib/utils/create-basic-auth-cf-fn.function.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals'
+import { describe, expect, it } from 'vitest'
 
 import type { CloudFrontFunctionEvent } from './cloudfront-function-request.type.js'
 import { createBasicAuthCfFn } from './create-basic-auth-cf-fn.function.js'
